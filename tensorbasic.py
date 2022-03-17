370be0d618d7ced75030bfb1e3a14608f5d6d00f
dleegithub <75201651+dleegithub@users.noreply.github.com>
2022-03-17 11:31:10 -0500

    Add files via upload

---
+++ tensorbasic.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+import numpy as np
+t1 = [[1, 2, 3], [4, 5, 6]]
+t2 = [[7, 8, 9], [10, 11, 12]]
+result =  tf.concat([t1, t2], 0)
+np.savetxt("test.csv", result, fmt='%.2f', delimiter=",")
